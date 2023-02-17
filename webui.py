7929424e0f85736fdc4590d8768b83d1c3a7cff7
Morten Gulden <morteng@gmail.com>
2023-02-17 10:55:21 +0100

    test

--- webui.py
+++ webui.py
@@ -48,13 +48,7 @@ import modules.hypernetworks.hypernetwork
 #it override by Morten for production server
 #cmd_opts.listen = True
 cmd_opts.api = True
-cmd_opts.api_log = True
-cmd_opts.cors_allow_origins_regex = r"^/api/.*$"
-cmd_opts.gradio_queue = True
-cmd_opts.autolaunch = True
-cmd_opts.theme = "dark"
-cmd_opts.api_auth  = "1:1"
-cmd_opts.api_log  = True
+
 
 #--api --api-auth 1:1 --api-log --cors-allow-origins-regex * --gradio-queue --autolaunch --theme dark 
 
